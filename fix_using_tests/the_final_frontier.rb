d2b3dd465351bafbbfed662dc82c1b2233260056
Mathew E Newton <elijah2895@hotmail.com>
2021-02-02 03:47:27 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -9,7 +10,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
+def engage(date = "Captain's Log")
   puts state_log(date)
-  date = generate_star_date
+  date == generate_star_date
 end
